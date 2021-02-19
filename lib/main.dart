23ca77e8c21769324b42f4eca055926b06063c6a
sorhajy <sorhajy@icloud.com>
2021-02-19 20:19:46 +0700

    Base UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,182 @@
+// import 'dart:html';
+// import 'dart:js';
+import 'dart:ui';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// Colors {
+//   background: #242629 / 0xFF242629
+//   message_background: #16161a / 0xFF16161A
+//   message_text: #fffffe / 0xFFFFFFFE
+//   message_detail: #94a1b2 / 0xFF94A1B2
+//   highight_purple: #7f5af0 / 0xFF7F5AF0
+//   tertiary: #2cb67d / 0xFF2CB67D
+//   error: #f25f4c / 0xFFF25F4C
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blueGrey,
+      home: Scaffold(
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          backgroundColor: Color(0xFF242629),
+          iconTheme:  IconThemeData(color: Color(0xFF2CB67D)),
+          leading: IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.menu,
+            ),
+          ),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.settings), onPressed: (){}),
+          ],
+        ),
+        backgroundColor: Color(0xFF242629),
+        body: Stack(
+          children:<Widget>[
+            Main_Page(),
+          ]
+        )
       ),
-      home: MyHomePage(title: 'Partisan'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Main_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Text(
-            //   'You have pushed the button this many times:',
-            // ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+    return Container(
+    child:Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children:<Widget>[
+      Expanded(flex:2, child: Container(child: Contacts()),),
+      Expanded(flex:10, child: Container(child: Chat()),), 
+    ],
+  ),
+  );
+  }
+}
+
+class Text_Message extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container (
+      margin: EdgeInsets.fromLTRB(1, 0, 5, 5),
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 0.5,
+              color: Color(0xFF94A1B2),
+              style: BorderStyle.solid
             ),
-          ],
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFF94A1B2),
+                blurRadius: 1.0,
+                spreadRadius: 1.0
+              )
+            ],
+            color: Colors.white
+          ),
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: TextField(
+            textAlign: TextAlign.start,
+            obscureText: false,
+          )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+dynamic _getContacts([bool append=false]){
+  // append ??= false;
+  int contactCount = 30;
+  if (append) {
+    contactCount += 5;
+  }
+  List arr = [];
+  Random rnd = new Random();
+  for (int i = 0; i<contactCount; i++) {
+    int r = 0 + rnd.nextInt(1000 - 0);
+    arr.add(r);
+    // print(arr[i]);
+  }
+
+  return arr;
+}
+
+class Contacts extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List chat_count = _getContacts();
+    // print(chat_count);
+    final List<String> items = List<String>.generate(chat_count.length, (index) => 'Chat with id ${chat_count[index]}');
+    return Container(
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('${items[index]}'),
+              leading: Icon(Icons.contact_page),
+            )
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class Chat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    child: Column(
+      children: <Widget>[
+      Expanded(
+        flex: 1,
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              // color: Colors.blue
+            ),
+            child: Container(
+              child: Column(
+                children: [
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                  Icon(Icons.text_fields,size:50),
+                ],
+              )
+            ),
+          )
+        )
+      ),
+    Text_Message(),
+    ],
+    )
     );
   }
 }
